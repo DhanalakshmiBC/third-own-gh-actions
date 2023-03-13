f48eeed703157d5cc3a5b6d06427eb0814d14428
vipulpathak113 <vipulpathak113@gmail.com>
2023-03-13 19:55:12 +0530

    user interaction event testing

--- src/__tests__/TestWithMockDataWithBranching.test.jsx
+++ src/__tests__/TestWithMockDataWithBranching.test.jsx
@@ -1,17 +1,41 @@
-import { render, screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
-import TestWithMockDataWithBranching from '../components/TestWithMockDataWithBranching'
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import TestWithMockDataWithBranching from "../components/TestWithMockDataWithBranching";
 import { mockData } from "../App";
 
 test("List renders successfully", () => {
-    render(<TestWithMockDataWithBranching data={mockData} displayUnorderedList={true} />)
+  render(
+    <TestWithMockDataWithBranching
+      data={mockData}
+      displayUnorderedList={true}
+    />
+  );
 
-    expect(screen.getByText(/fletcher/i)).toBeInTheDocument();
-
-})
+  expect(screen.getByText(/fletcher/i)).toBeInTheDocument();
+});
 
 test("Ordered list renders", () => {
-    render(<TestWithMockDataWithBranching data={mockData} displayUnorderedList={false} />)
+  render(
+    <TestWithMockDataWithBranching
+      data={mockData}
+      displayUnorderedList={false}
+    />
+  );
 
-    expect(screen.getByText(/McVanamy/i)).toBeInTheDocument();
-})
+  expect(screen.getByText(/McVanamy/i)).toBeInTheDocument();
+});
+
+test("Email link click handler called", async () => {
+  const mockFn = jest.fn();
+
+  render(
+    <TestWithMockDataWithBranching
+      data={mockData}
+      displayUnorderedList={true}
+      handleClick={mockFn}
+    />
+  );
+
+  await userEvent.click(screen.getByText(/mmcvanamy0@e-recht24.de/i));
+  expect(mockFn).toHaveBeenCalled();
+});
