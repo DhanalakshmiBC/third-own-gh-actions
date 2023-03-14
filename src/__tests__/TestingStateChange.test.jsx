f1ecbc819416a8226faf63d6734912ac1fc0782d
vipulpathak113 <vipulpathak113@gmail.com>
2023-03-14 21:01:46 +0530

    test state change on a button click

--- src/__tests__/TestingStateChange.test.jsx
+++ src/__tests__/TestingStateChange.test.jsx
@@ -1,4 +1,5 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import TestingStateChange from "../components/TestingStateChange";
 
 describe("TestingStateChange Component", () => {
@@ -6,5 +7,15 @@ describe("TestingStateChange Component", () => {
     render(<TestingStateChange />);
 
     expect(screen.getByText(/page loaded/i)).toBeInTheDocument();
+  });
+
+  test("Testing state change on button click", async () => {
+    render(<TestingStateChange />);
+
+    await waitFor(() => {
+      userEvent.click(screen.getByText(/toggle text/i));
+    });
+
+    expect(screen.getByText(/text visible/i)).toBeInTheDocument();
   });
 });
--- src/components/TestingStateChange.jsx
+++ src/components/TestingStateChange.jsx
@@ -2,12 +2,25 @@ import React, { useEffect, useState } from "react";
 
 const TestingStateChange = () => {
   const [loaded, setLoaded] = useState(false);
+  const [toggleTextVisible, setToggleTextVisible] = useState(false);
 
   useEffect(() => {
     setLoaded(true);
   }, []);
 
-  return <div>{loaded && <h3> Page Loaded </h3>}</div>;
+  return (
+    <div>
+      {loaded && <h3> Page Loaded </h3>}
+      {toggleTextVisible && <p> Text visible </p> }
+      <button
+        onClick={() => {
+          setToggleTextVisible(!toggleTextVisible);
+        }}
+      >
+        Toggle text
+      </button>
+    </div>
+  );
 };
 
 export default TestingStateChange;
